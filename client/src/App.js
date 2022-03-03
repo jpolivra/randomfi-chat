94668ec2517163757524eaa7cd3e612739c360c0
Joao Oliveira <thxjao.dev@gmail.com>
2022-03-03 19:29:11 -0300

    feat: enter room functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
 import "./App.css";
 import io from "socket.io-client";
+import { useState } from "react";
 
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  return <div className="App">hello</div>;
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h3>Join Chat</h3>
+      <input
+        type="text"
+        placeholder="Nickname"
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      ></input>
+      <input
+        type="text"
+        placeholder="Room"
+        onChange={(e) => {
+          setRoom(e.target.value);
+        }}
+      ></input>
+      <button onClick={joinRoom}>Join</button>
+    </div>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -18,6 +18,11 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log(`User Connected: ID - ${socket.id}`);
 
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined into room ${data}`);
+  });
+
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });

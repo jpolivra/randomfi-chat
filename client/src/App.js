c00ffdba57af5f0bfbaf84c2b1dc682d80419698
Joao Oliveira <thxjao.dev@gmail.com>
2022-03-09 19:29:39 -0300

    feat: added typescript to the project

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import "./App.css";
-import io from "socket.io-client";
 import { useState } from "react";
+import io from "socket.io-client";
+import "./App.css";
 
 const socket = io.connect("http://localhost:3001");
 
